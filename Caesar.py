de874e93caea9224f539cde492d0ad861cde1f44
egorrozinskiy <egor.rozinskiy@mail.ru>
2016-04-11 12:52:39 +0400

    Update and rename README.md to Caesar.py

--- Caesar.py
+++ Caesar.py
@@ -1,20 +1,6 @@
 __author__ = 'Timofey Khirianov'
 # -*- coding: utf8 -*-
 
-class Atbash:
-    alphabet = "я 	ю 	э 	ь 	ы 	ъ 	щ 	ш 	ч 	ц 	х 	ф 	у 	т 	с 	р 	п 	о 	н 	м 	л 	к 	й 	и 	з 	ж 	ё 	е 	д 	г 	в 	б 	а"
-
-    def __init__(self):
-        lowercase_code = {self.alphabet[i]:self.alphabet[-i-1] for i in range(len(self.alphabet))}
-        uppercase_code = {self.alphabet[i].upper():self.alphabet[-i-1].upper() for i in range(len(self.alphabet))}
-        self._encode = dict(lowercase_code)
-        self._encode.update(uppercase_code)
-
-    def encode(self, line):
-        if len(line) == 1:
-            return self._encode[line] if line in self._encode else line
-        else:
-            return ''.join([self.encode(char) for char in line])
 
 
 class Caesar:
